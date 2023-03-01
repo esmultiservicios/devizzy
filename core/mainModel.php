5fccfeb33fb2ffe70b9b7fb54e35db88572012ae
soycristhianflores <69639311+soycristhianflores@users.noreply.github.com>
2023-03-01 12:26:56 -0600

    envio correo
    formato de correo

--- core/mainModel.php
+++ core/mainModel.php
@@ -5916,7 +5916,7 @@
 			$mysqli_main->set_charset("utf8");
 		
 			if ($mysqli_main->connect_errno) {
-			   echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
+			   echo "Fallo al conectar a MySQL: " . $mysqli_main->connect_error;
 			   exit;
 			}
 		
