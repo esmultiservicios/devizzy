873f14dab4a59103cf48f3dd79927fc4caf63d37
CLINICARE <edwin.velasquez@clinicarehn.com>
2023-10-01 12:22:50 -0600

    cambios en el estilo y en el menu top
    cambios en el estilo y en el menu top

--- vistas/plantilla/modulos/navbartop.php
+++ vistas/plantilla/modulos/navbartop.php
@@ -6,36 +6,62 @@
     <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i
             class="fas fa-bars fa-lg"></i></button>
 
-    <a class="nav-link link menu-item reporteVentas" href="<?php echo SERVERURL; ?>reporteVentas/" style="display:none">
-        <div class="sb-nav-link-icon"></i></div>Reporte Ventas
-    </a>
-    <a class="nav-link link menu-item reporteCotizacion" href="<?php echo SERVERURL; ?>reporteCotizacion/"
-        style="display:none">
-        <div class="sb-nav-link-icon"></div>Reporte Cotización
-    </a>
-    <a class="nav-link link menu-item reporteCompras" href="<?php echo SERVERURL; ?>reporteCompras/"
-        style="display:none"></i></div>Reporte Compras</a>
-
-    <a class="nav-link link menu-item cobrarClientes" href="<?php echo SERVERURL; ?>cobrarClientes/"
-        style="display:none">
-        <div class="sb-nav-link-icon"></div>CXC Clientes
-    </a>
-    <a class="nav-link link menu-item pagarProveedores" href="<?php echo SERVERURL; ?>pagarProveedores/"
-        style="display:none">
-        <div class="sb-nav-link-icon"></div>CXP Proveedores
-    </a>
-    <a class="nav-link link menu-item inventario" href="<?php echo SERVERURL; ?>inventario/" style="display:none">
-        <div class="sb-nav-link-icon"></div>Movimientos
-    </a>
-    <a class="nav-link link menu-item transferencia" href="<?php echo SERVERURL; ?>transferencia/" style="display:none">
-        <div class="sb-nav-link-icon"></div>Inventario
-    </a>
-    <a class="nav-link link menu-item nomina" href="<?php echo SERVERURL; ?>nomina/" style="display:none">
-        <div class="sb-nav-link-icon"></div>Nomina
-    </a>
-    <a class="nav-link link menu-item asistencia" href="#" id="marcarAsistencia">
-        <div class="sb-nav-link-icon"></div>Asistencia
-    </a>
+    <!-- Menú principal -->
+    <ul class="navbar-nav">
+        <!-- Elementos del menú principal -->
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item reporteVentas" href="<?php echo SERVERURL; ?>reporteVentas/"
+                style="display:none">
+                <div class="sb-nav-link-icon"></div>Reporte Ventas
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item reporteCotizacion" href="<?php echo SERVERURL; ?>reporteCotizacion/"
+                style="display:none">
+                <div class="sb-nav-link-icon"></div>Reporte Cotización
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item reporteCompras" href="<?php echo SERVERURL; ?>reporteCompras/"
+                style="display:none">
+                <div class="sb-nav-link-icon"></div>Reporte Compras
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item cobrarClientes" href="<?php echo SERVERURL; ?>cobrarClientes/"
+                style="display:none">
+                <div class="sb-nav-link-icon"></div>CXC Clientes
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item pagarProveedores" href="<?php echo SERVERURL; ?>pagarProveedores/"
+                style="display:none">
+                <div class="sb-nav-link-icon"></div>CXP Proveedores
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item inventario" href="<?php echo SERVERURL; ?>inventario/"
+                style="display:none">
+                <div class="sb-nav-link-icon"></div>Movimientos
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item transferencia" href="<?php echo SERVERURL; ?>transferencia/"
+                style="display:none">
+                <div class="sb-nav-link-icon"></div>Inventario
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item nomina" href="<?php echo SERVERURL; ?>nomina/" style="display:none">
+                <div class="sb-nav-link-icon"></div>Nomina
+            </a>
+        </li>
+        <li class="nav-item d-none d-md-block d-sm-block">
+            <a class="nav-link link menu-item asistencia" href="#" id="marcarAsistencia">
+                <div class="sb-nav-link-icon"></div>Asistencia
+            </a>
+        </li>
+    </ul>
 
     <div class="dropdown d-md-none">
         <button class="btn btn-secondary bg-color-navarlateral dropdown-toggle" type="button" id="dropdownMenuButton"
