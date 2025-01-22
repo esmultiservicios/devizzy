4c0f45679cd50dfe71a1aca58f6a48bec0a4787c
Edwin Velasquez <edwin.velasquez@clinicarehn.com>
2025-01-22 11:11:25 -0600

    se agrego el formato de documento
    se agrego el formato de documento

--- core/cotizacion.php
+++ core/cotizacion.php
@@ -9,7 +9,7 @@ use LDAP\Result;
 <head>
     <meta charset="UTF-8">
     <title>Cotziación</title>
-    <link rel="stylesheet" href="<?php echo SERVERURL; ?>vistas/plantilla/css/style_factura.css">
+    <link rel="stylesheet" href="<?php echo SERVERURL; ?>vistas/plantilla/css/style_factura_carta.css">
     <link rel="shortcut icon" href="<?php echo SERVERURL; ?>vistas/plantilla/img/icono.png">
 </head>
 
